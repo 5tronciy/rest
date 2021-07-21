d74094b5a609ae9fd03a6595301db114c3d636fe
5tronciy <romangolchuk@gmail.com>
2021-07-21 10:33:31 +0300

    add queries showTables, tableMeta, rowById

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 require("dotenv").config();
-var mysql = require("mysql");
-var connection = mysql.createConnection({
+const mysql = require("mysql");
+const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: process.env.PASSWORD,
   database: "university",
 });
 
+const showTables = () => {
+  connection.query("SHOW TABLES;", function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+  });
+};
+
+const tableMeta = (tableName) => {
+  connection.query("DESCRIBE " + tableName, function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+  });
+};
+
+const rowById = (tableName, id) => {
+  connection.query(
+    "SELECT * FROM " + tableName + " WHERE id=?",
+    [id],
+    function (error, results, fields) {
+      if (error) throw error;
+      console.log(results);
+    }
+  );
+};
+
 connection.connect();
 
-connection.query("SHOW TABLES;", function (error, results, fields) {
-  if (error) throw error;
-  console.log(results);
-});
+showTables();
+
+// tableMeta("instructor");
+
+// rowById("instructor", 76766);
 
 connection.end();
