9546fbe9f963d86db371beadafd24077264c6b4d
5tronciy <romangolchuk@gmail.com>
2021-07-28 14:55:57 +0300

    add get methods

--- server.js
+++ server.js
@@ -2,15 +2,44 @@ const express = require("express");
 const app = express();
 const port = 10000;
 
-const { showTables } = require("./services/db");
+const {
+  getMetadata,
+  getTableMetadata,
+  getAll,
+  getById,
+} = require("./services/db");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/app/rest/metadata", (req, res) => {
-  const tables = showTables();
-  res.send(tables);
+app.get("/rest/metadata", (req, res) => {
+  getMetadata((tables) => {
+    res.send(JSON.stringify(tables));
+  });
+});
+
+app.get("/rest/:table/metadata", (req, res) => {
+  const { table } = req.params;
+  getTableMetadata(table, (metadata) => {
+    res.send(JSON.stringify(metadata));
+  });
+});
+
+app.get("/rest/:table", (req, res) => {
+  const { table } = req.params;
+  getAll(table, (data) => {
+    res.send(JSON.stringify(data));
+  });
+});
+
+app.get("/rest/:table/:id", (req, res) => {
+  const { table, id } = req.params;
+  getById(table, id, (row) => {
+    res.send(JSON.stringify(row));
+  });
 });
 
 app.listen(port, () => {
